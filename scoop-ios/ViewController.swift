be7927811f1b869a7c0dde7eb727ddda07883106
Shane Alton <shanealton1@gmail.com>
2021-10-12 14:10:20 -0400

    Initial Commit

---
+++ scoop-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scoop-ios
+//
+//  Created by Shane Alton on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
